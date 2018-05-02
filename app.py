d65bca517134edd15e57be9b912c463188f5adb4
jtaam <tamjid.mdh@gmail.com>
2018-05-03 03:38:31 +0600

    uppercase

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,19 +13,29 @@ class City(db.Model):
     name = db.Column(db.String(50), nullable=False)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    cities = City.query.all()
+    # Add city
+    if request.method == 'POST':
+        new_city = request.form.get('city')
+        if new_city:
+            new_city_obj = City(name=new_city)
+
+            db.session.add(new_city_obj)
+            db.session.commit()
+
+    # Fetch & show data
+    cities = City.query.order_by('-id').all()
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=22e798af81a0e446f923a9745766b7e6'
 
     weather_data = []
 
     for city in cities:
-        r = requests.get(url.format(city)).json()
+        r = requests.get(url.format(city.name)).json()
         # print(r)
         weather = {
-            'city': city,
+            'city': city.name,
             'temparature': r['main']['temp'],
             'description': r['weather'][0]['description'],
             'icon':r['weather'][0]['icon'],
